8300ff4d0cc0e024ecfc42e872dbeb6dd2a0cc5b
DavidTHE-GitMenace <dvdjrocks480@gmail.com>
2026-05-15 01:17:45 -0600

    Added Top Bar, still working on side bar.

--- flowboard/src/components/layout/Sidebar.tsx
+++ flowboard/src/components/layout/Sidebar.tsx
@@ -1,6 +1,46 @@
-function SideBar () {
-    return <div className="line2"></div>
+import {House} from "lucide-react";
+import {Settings} from "lucide-react";
+import {ChartColumnBig} from "lucide-react";
+import {Pencil} from "lucide-react";
+import {Siren} from "lucide-react";
+import {ClockFading} from "lucide-react";
+import {Clipboard} from "lucide-react";
 
+function SideBar () {
+    return ( 
+        <header className="line2">
+
+            <button className="HomeButton">
+                <House size={70} />
+            </button>
+
+            <button className="SettingsButton">
+                <Settings size={70} />
+            </button>
+
+            <button className="ChartButton">
+                <ChartColumnBig size={70} />
+            </button>
+
+            <button className="PencilButton">
+                <Pencil size={70} />
+            </button>
+
+            <button className="SirenButton">
+                <Siren size={70} />
+            </button>
+
+            <button className="ClockButton">
+                <ClockFading size={70} />
+            </button>
+
+            <button className="TasksButton">
+                <Clipboard size={70} />
+            </button>
+
+        </header>
+
+    );
 }
 
 export default SideBar;
